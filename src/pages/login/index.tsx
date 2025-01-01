d2a2d34f0c266e93666577e52aebf7470969d829
Dionovan Ramadhani <80899280+kizaruu27@users.noreply.github.com>
2025-01-01 23:19:35 +0700

    update login page styling with SASS

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,7 +1,8 @@
 import styles from "@/styles/login/login.module.scss";
-import keeb1 from "../../../public/images/keeb1.svg";
-import keeb2 from "../../../public/images/keeb2.svg";
-import keeb3 from "../../../public/images/keeb3.svg";
+import keeb1 from "../../../public/images/login/keeb1.avif";
+import keeb2 from "../../../public/images/login/keeb2.avif";
+import keeb3 from "../../../public/images/login/keeb3.avif";
+import keeb4 from "../../../public/images/login/keeb4.avif";
 import logo from "../../../public/images/logo.svg";
 import google from "../../../public/images/google.svg";
 import Image from "next/image";
@@ -9,62 +10,102 @@ import Link from "next/link";
 
 export default function LoginPage() {
   return (
-    <div className="flex w-full h-[100vh]">
-      {/* Keyboard images */}
-      <div className="w-[50%] grid grid-cols-2 gap-4 p-5">
-        <div className="row-span-2">
-          <Image
-            src="https://res.cloudinary.com/kineticlabs/image/upload/q_auto/c_fit,w_1000/f_auto/v1/api-images/home/social-grid/long/DSC09584_1_a1lrit"
-            alt="keeb1"
-            width={500}
-            height={500}
-            className="object-cover h-full w-full rounded-xl"
-          />
-        </div>
-        <div className="row-span-1">
-          <Image
-            src="https://res.cloudinary.com/kineticlabs/image/upload/q_auto/c_fit,w_1000/f_auto/v1/api-images/home/social-grid/short/peng_close_4_pwgtdy"
-            alt="keeb2"
-            width={500}
-            height={500}
-            className="object-cover h-full w-full rounded-xl"
-          />
-        </div>
-        <div className="row-span-1">
-          <Image
-            src="https://res.cloudinary.com/kineticlabs/image/upload/q_auto/c_fit,w_1000/f_auto/v1/api-images/home/social-grid/short/DSCF7666_1_yy3itw"
-            alt="keeb2"
-            width={500}
-            height={500}
-            className="object-cover h-full w-full rounded-xl"
-          />
+    <div className={styles.login}>
+      {/* Images Grid */}
+      <div className={styles.login__imageSection}>
+        <div className={styles.login__imageSection__imageContainer}>
+          <div
+            className={styles.login__imageSection__imageContainer__multipleImageContainer}
+          >
+            <div
+              className={
+                styles.login__imageSection__imageContainer__multipleImageContainer__left
+              }
+            >
+              <Image
+                src={keeb1}
+                alt="keeb-1"
+                className={
+                  styles.login__imageSection__imageContainer__multipleImageContainer__left__image
+                }
+              />
+            </div>
+            <div
+              className={
+                styles.login__imageSection__imageContainer__multipleImageContainer__right
+              }
+            >
+              <Image
+                src={keeb2}
+                alt="keeb-2"
+                className={
+                  styles.login__imageSection__imageContainer__multipleImageContainer__right__image
+                }
+              />
+              <Image
+                src={keeb3}
+                alt="keeb-3"
+                className={
+                  styles.login__imageSection__imageContainer__multipleImageContainer__right__image
+                }
+              />
+            </div>
+          </div>
+          <div
+            className={styles.login__imageSection__imageContainer__singleImageContainer}
+          >
+            <Image
+              src={keeb4}
+              alt="keeb-4"
+              className={
+                styles.login__imageSection__imageContainer__singleImageContainer__image
+              }
+            />
+          </div>
         </div>
       </div>
-      {/* Login inputs */}
-      <div className="flex-1 flex justify-center items-center">
-        <div className="bg-[#EEEEEE] w-[60%] h-[80%] rounded-2xl p-8 flex flex-col justify-between">
-          <Image src={logo} alt="" className="w-52" />
-          <form action="" className="mt-5">
-            <div className="flex flex-col gap-1 mb-5">
-              <label htmlFor="email" className="text-[#848484]">
+      <div className={styles.login__formSection}>
+        <div className={styles.login__formSection__formContainer}>
+          <Image
+            src={logo}
+            alt=""
+            className={styles.login__formSection__formContainer__logo}
+          />
+          <form action="" className={styles.login__formSection__formContainer__form}>
+            <div className={styles.login__formSection__formContainer__form__formItem}>
+              <label
+                htmlFor="email"
+                className={
+                  styles.login__formSection__formContainer__form__formItem__label
+                }
+              >
                 email
               </label>
               <input
                 type="email"
                 name="email"
                 id="email"
-                className="p-2 rounded-lg bg-[#D9D9D9]"
+                className={
+                  styles.login__formSection__formContainer__form__formItem__input
+                }
               />
             </div>
-            <div className="flex flex-col gap-1 mb-5">
-              <label htmlFor="password" className="text-[#848484]">
+            <div className={styles.login__formSection__formContainer__form__formItem}>
+              <label
+                htmlFor="password"
+                className={
+                  styles.login__formSection__formContainer__form__formItem__label
+                }
+              >
                 password
               </label>
               <input
                 type="password"
                 name="password"
                 id="password"
-                className="p-2 rounded-lg bg-[#D9D9D9]"
+                className={
+                  styles.login__formSection__formContainer__form__formItem__input
+                }
               />
             </div>
           </form>
